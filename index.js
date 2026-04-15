342202db802f77b5a1d1877913f463cbc12966fa
lysa omondi <lisabethkidangasi@gmail.com>
2026-04-15 06:58:11 +0300

    my test

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code here!
+const postList = document.getElementById("post-list");
+function displayPosts(posts) {
+  posts.forEach(post => {
+    const li = document.createElement("li");
+    const title = document.createElement("h1");
+    const body = document.createElement("p");
+ title.textContent = post.title;
+    body.textContent = post.body;
+    li.appendChild(title);
+    li.appendChild(body);
+    postList.appendChild(li);
+  });
+}
+async function getPosts() {
+  try { const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+ const posts = await response.json();
+console.log(posts);
+ displayPosts(posts);
+
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+  }
+}
+getPosts();
